43b8bcc6ff40ac4305e4588333b9d9b3dee7de5f
pfulcher26 <pfulcher26@gmail.com>
2022-07-30 00:23:50 -0700

    Adds the beggining of render function and creates a seperate render board function that assigns the value of the current board tile to the colors object value that is stored in the board array.

--- js/main.js
+++ js/main.js
@@ -16,8 +16,63 @@ const winningCombinations = {
     eighth: [2, 4, 6]
 }
 
-console.log(winningCombinations.first);
 //variables
 let boardArray = ['', '', '', '', '', '', '', '', '',];
 let currentPlayer = "";
 let winner = "";
+
+//reusable elements
+//these represent the board squares 
+const boardTiles = {
+    tile1: "",
+    tile2: "",
+    tile3: "",
+    tile4: "",
+    tile5: "",
+    tile6: "",
+    tile7: "",
+    tile8: "",
+    tile9: ""
+}
+
+//initialize state variables
+function initialize() {
+    boardArray = [null, null, null, null, null, null, null, null, null, null];
+    currentPlayer = 1;
+    winner = null;
+    render();
+}
+
+function render() {
+    renderBoard();
+}
+
+function rendBoard() {
+    for(i = 0; i < boardTiles.length; i++) {
+        for(j = 0; j < boardArray.length; j++) {
+            let color = boardTiles[i[boardArray[j]]] = colors.value; 
+        }
+    }
+}
+// couldn't get this logic to work, so hard coded the nulls
+// function initializeBoard() {
+//     boardArray.forEach((element) => { 
+//         return element = null;
+// })}
+
+initializeBoard();
+
+ 
+
+
+
+
+//render state variables to the page
+    //render the board
+    //render a message
+    //wait for the user to click a square
+
+
+//handle a player clicking a square
+
+//handle a player clicking the replay button
