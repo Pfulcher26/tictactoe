e439033762f45e1197f28527f49b11182cee01c3
pfulcher26 <pfulcher26@gmail.com>
2022-07-29 17:04:10 -0700

    Adds an object with arrays that pertain to possible winning combinations indexed from 0.

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//constants
+const colors = {
+    null: 'white',
+    playerOne: 'red',
+    playerMinusOne: 'blue'
+};
+
+const winningCombinations = {
+    first: [0, 1, 2],
+    second: [3, 4, 5],
+    third: [6, 7, 8],
+    fourth: [0, 3, 6],
+    fifth: [1, 4, 7],
+    sixth: [2, 5, 8],
+    seventh: [0, 4, 8],
+    eighth: [2, 4, 6]
+}
